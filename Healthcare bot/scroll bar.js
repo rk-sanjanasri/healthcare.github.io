96cbb2ae24e5c192b9415882c8a2bf3d3a986534
rk-sanjanasri <ssri40412@gmail.com>
2026-02-18 08:27:25 +0530

    Delete Healthcare bot directory

--- Healthcare bot/scroll bar.js
+++
@@ -1,7 +0,0 @@
-function sendMessage() {
-    // ... your existing code ...
-
-    // ADD THIS AT THE VERY END OF THE FUNCTION:
-    const chatArea = document.getElementById("chatArea");
-    chatArea.scrollTop = chatArea.scrollHeight; 
-}
