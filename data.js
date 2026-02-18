e3d3e35283ed15f98c11efb2a8cd7e2b22b834c6
rk-sanjanasri <ssri40412@gmail.com>
2026-02-18 08:27:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+// data.js
+const medicalData = [
+    {
+        "disease": "Common Cold",
+        "symptoms": ["cough", "sneezing", "sore throat", "runny nose"],
+        "advice": "Rest, stay hydrated, and use saline drops for congestion."
+    },
+    {
+        "disease": "Flu",
+        "symptoms": ["fever", "chills", "body aches", "fatigue", "headache"],
+        "advice": "Stay home, rest, and stay hydrated. Consult a doctor if fever stays high."
+    },
+{
+"disease":"Dengue",
+"symptoms":["fatigue", "fever", "shivering", "chills", "nausea"],
+"advice":"Visit a doctor and get a blood test. Take plenty of fluid and rest."
+},
+{
+"disease":"Heart attack",
+"symptoms":["chest pain", "difficulty in breathing"],
+"advice":"Stay calm! Call an ambulance and go to the hospital immediately!"
+}
+];
